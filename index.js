69d7dfd01037c43a0d38f572bff699fab8afbac3
chaoseternai <jamesho.AHS@gmail.com>
2018-04-23 18:02:51 -0700

    Added ally refractors

--- index.js
+++ index.js
@@ -4,28 +4,31 @@ const API_KEY = 'AIzaSyBgV3q8M71c-Hkqh3VmKzJSDlnoidrmuzM';
 function searchYoutube() {
   $('#search-form').submit(function(event) {
     event.preventDefault();
-    
+
     const query = {
       part: 'snippet',
       key: API_KEY,
       q: $('#search').val()
     }
-    
-    let results = $.getJSON(YOUTUBE_URL, query, showResults)  
+
+    let results = $.getJSON(YOUTUBE_URL, query, showResults)
   });
 }
 
 function showResults(data) {
   let videoList = $('a');
-  
+
   console.log(data);
-  
+
   for(let i=0; i<5; i++) {
     $(videoList[i]).attr('href', `https://youtube.com/watch?v=${data.items[i].id.videoId}`);
     $(videoList[i]).children('img').attr('src', data.items[i].snippet.thumbnails.high.url);
   }
-  
+
   $('#search').val('');
+
+  $('#results-text, #videos').removeAttr('hidden');
+  $('#results-text').html('5 videos displayed');
 }
 
 $(searchYoutube);
